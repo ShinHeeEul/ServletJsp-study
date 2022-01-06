af313b8c5f9c5a11667d36c979913b967679ad84
ShinHeeEul <sheshe7015@gmail.com>
2022-01-06 14:16:09 +0900

    Test

--- JSPPrj/src/main/java/com/newlecture/web/Nana.java
+++ JSPPrj/src/main/java/com/newlecture/web/Nana.java
@@ -17,6 +17,6 @@ public class Nana extends HttpServlet {
 		protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 			
 			PrintWriter out = resp.getWriter();
-			out.println("Hello Servlet!1");
+			out.println("Hello Servlet!@@");
 		}
 }
