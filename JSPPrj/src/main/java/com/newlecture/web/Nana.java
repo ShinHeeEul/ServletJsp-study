cc3c20e2e25d3b5a207d29153834835d00596ddd
ShinHeeEul <sheshe7015@gmail.com>
2022-01-06 15:10:01 +0900

    2022-01-06
    Get request & query string

--- JSPPrj/src/main/java/com/newlecture/web/Nana.java
+++ JSPPrj/src/main/java/com/newlecture/web/Nana.java
@@ -13,13 +13,21 @@ import javax.servlet.http.HttpServletResponse;
 /*
  * 한글이 깨지는 이유
  * 서버에서 한글을 지원하지 않는 문자코드로 인코딩을 했거나 브라우저가 다른 문자코드로 이해한 경우
+ * 
+ * Get request * query string
+ * - http://localhost/hello?cnt=3 -> 쿼리문을 get 방식으로 보낼 수 있다.
+ * 	-> 동적인 문서를 제작할 수 있다.
  * */
 
 @WebServlet("/hello")
 public class Nana extends HttpServlet {
 		
 		@Override
-		protected void service(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
+		protected void service(
+				//사용자로부터 요청을 받는 패킷
+				HttpServletRequest request, 
+				//사용자에게 보내주는 패킷
+				HttpServletResponse response) throws ServletException, IOException {
 			
 			//사용자에게 보내는 문자 코드 지정
 			response.setCharacterEncoding("UTF-8");
@@ -36,10 +44,13 @@ public class Nana extends HttpServlet {
 			
 			
 			PrintWriter out = response.getWriter();
-			for(int i = 0; i<100; i++) {
+			
+			//request로부터 쿼리문을 가져옴
+			int cnt = Integer.parseInt(request.getParameter("cnt"));
+			
+			for(int i = 0; i<cnt; i++) {
 				
 				out.println((i+1) + " : 안녕 서블릿!@@ <br>");
-				out.println("test");
 				
 //				out.println((i+1) + " : 안녕 서블릿!@@");
 
