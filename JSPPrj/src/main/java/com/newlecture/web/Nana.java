5a7b4b2505ac1d56da2f65b0905d0a9fffa44eb1
ShinHeeEul <sheshe7015@gmail.com>
2022-01-06 14:19:01 +0900

    2022-01-06
    set text type
    text type & text code

--- JSPPrj/src/main/java/com/newlecture/web/Nana.java
+++ JSPPrj/src/main/java/com/newlecture/web/Nana.java
@@ -10,13 +10,37 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
+/*
+ * 한글이 깨지는 이유
+ * 서버에서 한글을 지원하지 않는 문자코드로 인코딩을 했거나 브라우저가 다른 문자코드로 이해한 경우
+ * */
+
 @WebServlet("/hello")
 public class Nana extends HttpServlet {
 		
 		@Override
-		protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		protected void service(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
 			
-			PrintWriter out = resp.getWriter();
-			out.println("Hello Servlet!@@");
+			//사용자에게 보내는 문자 코드 지정
+			response.setCharacterEncoding("UTF-8");
+			//어떻게 해석할 것인가를 지정(html문서이며, UTF-8 문자 코드로 읽는다)
+			
+			/*
+			 * HTML로 출력시 : setContentType("text/html");
+			 * 일반 TEXT로 출력시 : setContentType("text/plain");
+			 * 이미지 GIF로 출력시 : setContentType("image/gif");
+			 * 이미지 JPG, JPEG로 출력시 : setContentType("image/jpeg");
+			 **/
+			
+			response.setContentType("text/plain; charset=UTF-8");
+			
+			
+			PrintWriter out = response.getWriter();
+			for(int i = 0; i<100; i++) {
+				
+//				out.println((i+1) + " : 안녕 서블릿!@@ <br>");
+				
+				out.println((i+1) + " : 안녕 서블릿!@@");
+				 			}
 		}
 }
