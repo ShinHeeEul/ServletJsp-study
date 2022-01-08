b8d1875dc413c07de5bd858d1fc31d52e4d438d2
ShinHeeEul <sheshe7015@gmail.com>
2022-01-08 22:24:29 +0900

    2022-01-08
    state save with session object
    difference application & session

--- JSPPrj/src/main/java/com/newlecture/web/test/Calc2.java
+++ JSPPrj/src/main/java/com/newlecture/web/test/Calc2.java
@@ -9,32 +9,40 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/calc2")
 
 public class Calc2 extends HttpServlet {
 
+	
 	@Override
-	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+
 		
-		
-		
-		PrintWriter out = res.getWriter();
+		PrintWriter out = resp.getWriter();
 		String v_ = req.getParameter("value");
 		String op = req.getParameter("operator");
 		
 		ServletContext application = req.getServletContext(); //application 저장소
-		
+		HttpSession session = req.getSession();
+				
 		int v = 0;
 		
 		if(!v_.equals("")) v= Integer.parseInt(v_);
 		
 		if(op.equals("=")) {
 			
-			int x = (Integer) application.getAttribute("value");
+			//int x = (Integer) application.getAttribute("value");
+			int x = (Integer) session.getAttribute("value");
+			
 			int y = v;
-			String operator = (String) application.getAttribute("op");
+			//String operator = (String) application.getAttribute("op");
+			String operator = (String) session.getAttribute("op");
+			
 			int result = 0;
 			
 			if(operator.equals("+"))
@@ -48,10 +56,14 @@ public class Calc2 extends HttpServlet {
 			
 		}
 		else {
-			application.setAttribute("value", v); //map 컬렉션과 비슷한 느낌
-			application.setAttribute("op", op);
+			//application.setAttribute("value", v); //map 컬렉션과 비슷한 느낌
+			//application.setAttribute("op", op);
+			
+			session.setAttribute("value", v); //map 컬렉션과 비슷한 느낌
+			session.setAttribute("op", op);
 		}
+	}
+
 		
-	}	
 
 }
