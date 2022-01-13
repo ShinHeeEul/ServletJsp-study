201e011cea8e999a0831a6ef3ddf31873c2a59cc
ShinHeeEul <sheshe7015@gmail.com>
2022-01-13 22:19:11 +0900

    2022-01-13
    change detail page with MVC model2

---
+++ JSPPrj/src/main/java/com/newlecture/web/controller/NoticeDetailController.java
@@ -0,0 +1,72 @@
+package com.newlecture.web.controller;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/notice/detail")
+public class NoticeDetailController extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		int id = Integer.parseInt(req.getParameter("id"));
+		
+		String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
+		String sql = "SELECT * FROM NOTICE WHERE ID=?";
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con = DriverManager.getConnection(url,"\"Weed\"", "southkorea1");
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setInt(1, id);
+			ResultSet rs = st.executeQuery();
+			
+			rs.next();
+			
+			String title = rs.getString("TITLE");
+			Date regDate = rs.getDate("REGDATE");
+			String writerId = rs.getString("WRITER_ID");
+			String hit = rs.getString("HIT");
+			String files = rs.getString("FILES");
+			String content = rs.getString("CONTENT");
+			
+			req.setAttribute("title", title);
+			req.setAttribute("regDate", regDate);
+			req.setAttribute("writerId", writerId);
+			req.setAttribute("hit", hit);
+			req.setAttribute("files", files);
+			req.setAttribute("content", content);
+
+			rs.close();
+			st.close();
+			con.close();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+	
+		//redirect
+		//아예 다른 페이지로 가버리는 방법 -> 아예 로그인 페이지로 가거나, 게시글 등록 후 목록 페이지로 가기
+		
+		//foward
+		//작업했던 내용들 이어줘서 계속 작업할 수 있게
+		req.getRequestDispatcher("/notice/detail.jsp").forward(req, resp);
+		
+	}
+}
--- JSPPrj/src/main/webapp/notice/detail.jsp
+++ JSPPrj/src/main/webapp/notice/detail.jsp
@@ -1,38 +1,6 @@
-<%@page import="java.sql.Date"%>
-<%@page import="java.sql.PreparedStatement"%>
-<%@page import="java.sql.ResultSet"%>
-<%@page import="java.sql.Statement"%>
-<%@page import="java.sql.DriverManager"%>
-<%@page import="java.sql.Connection"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
-	
-	<%
-	
-	int id = Integer.parseInt(request.getParameter("id"));
-	
-	String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
-	String sql = "SELECT * FROM NOTICE WHERE ID=?";
-	
-	Class.forName("oracle.jdbc.driver.OracleDriver");
-	Connection con = DriverManager.getConnection(url,"\"Weed\"", "southkorea1");
-	PreparedStatement st = con.prepareStatement(sql);
-	st.setInt(1, id);
-	ResultSet rs = st.executeQuery();
-	
-	rs.next();
-	
-	String title = rs.getString("TITLE");
-	Date regDate = rs.getDate("REGDATE");
-	String writerId = rs.getString("WRITER_ID");
-	String hit = rs.getString("HIT");
-	String files = rs.getString("FILES");
-	String content = rs.getString("CONTENT");
 
-	rs.close();
-	st.close();
-	con.close();
-    %>
 
 <!DOCTYPE html>
 <html>
@@ -184,24 +152,24 @@
 							<tbody>
 								<tr>
 									<th>제목</th>
-									<td class="text-align-left text-indent text-strong text-orange" colspan="3"><%=title %></td>
+									<td class="text-align-left text-indent text-strong text-orange" colspan="3">${title}</td>
 								</tr>
 								<tr>
 									<th>작성일</th>
-									<td class="text-align-left text-indent" colspan="3"><%=regDate %></td>
+									<td class="text-align-left text-indent" colspan="3">${regDate}</td>
 								</tr>
 								<tr>
 									<th>작성자</th>
-									<td><%=writerId %></td>
+									<td>${writerId}</td>
 									<th>조회수</th>
-									<td><%=hit %></td>
+									<td>${hit}</td>
 								</tr>
 								<tr>
 									<th>첨부파일</th>
-									<td colspan="3"><%=files %></td>
+									<td colspan="3">${files}</td>
 								</tr>
 								<tr class="content">
-									<td colspan="4"><%=content %></td>
+									<td colspan="4">${content}</td>
 								</tr>
 							</tbody>
 						</table>
