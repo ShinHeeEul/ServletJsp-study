ae75d73b1daf7aa55e25b4bfc27c4284829ae2d4
ShinHeeEul <sheshe7015@gmail.com>
2022-01-15 22:45:26 +0900

    2022-01-15
    write SQL query of getNoticeList method

---
+++ JSPPrj/src/main/java/com/newlecture/web/service/NoticeService.java
@@ -0,0 +1,54 @@
+package com.newlecture.web.service;
+
+import java.util.List;
+
+import com.newlecture.web.entity.Notice;
+
+public class NoticeService {
+	public List<Notice> getNoticeList(){
+		
+		return getNoticeList("title","",1);
+	}
+	
+	public List<Notice> getNoticeList(int page){
+		
+		return getNoticeList("title", "", page);
+	}
+
+	public List<Notice> getNoticeList(String field, String query, int page){
+		String sql = "select * from("
+				+ "    select row_number() over (order by regdate desc) num,"
+				+ "    notice.* from notice"
+				+ ") where num between 6 and 10;";	
+		return null;
+	}
+	
+	public int getNoticeCount() {
+		
+		return getNoticeCount("title", "");
+	}
+	
+	public int getNoticeCount(String field, String query) {
+		
+		String sql = "select * from("
+				+ "    select row_number() over (order by regdate desc) num,"
+				+ "    notice.* from notice"
+				+ ") where num between 6 and 10;";	
+		
+		return 0;
+	}
+	
+	public Notice getNotice(int id) {
+		String sql = "select * from notice where id=?";
+		return null;
+	}
+	
+	public Notice getNextNotice(int id) {
+		return null;
+	}
+	
+	public Notice getprevNotice(int id) {
+		return null;
+	}
+
+}
