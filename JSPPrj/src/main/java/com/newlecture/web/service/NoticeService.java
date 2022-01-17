40c4911cb8af9db0c0501c3f38cd5d1a1e4a4b1d
ShinHeeEul <sheshe7015@gmail.com>
2022-01-17 22:12:20 +0900

    2022-01-17
    complete NoticeService class

--- JSPPrj/src/main/java/com/newlecture/web/service/NoticeService.java
+++ JSPPrj/src/main/java/com/newlecture/web/service/NoticeService.java
@@ -14,6 +14,9 @@ import com.newlecture.web.entity.Notice;
 
 public class NoticeService {
 	//page 보이게 구현하는 내용
+	
+	private String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
+	
 	public List<Notice> getNoticeList(){
 		
 		return getNoticeList("title","",1);
@@ -36,7 +39,7 @@ public class NoticeService {
 		
 		
 		
-		String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
+		
 		List<Notice> list = new ArrayList<>();
 		
 		try {
@@ -88,37 +91,212 @@ public class NoticeService {
 		return getNoticeCount("title", "");
 	}
 	
+	
 	public int getNoticeCount(String field, String query) {
 		
-		String sql = "select * from("
-				+ "    select row_number() over (order by regdate desc) num,"
-				+ "    notice.* from notice"
-				+ ") where num between 6 and 10;";
+		String sql = "select count(ID) count from ("
+				+ "    notice.* from notice where " + field +" like ? order by regdate desc"
+				+ ")";
+		
+		int count = 0;
+
+		List<Notice> list = new ArrayList<>();
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con = DriverManager.getConnection(url,"\"Weed\"", "southkorea1");
+			PreparedStatement st = con.prepareStatement(sql);
+			
+			st.setString(1, "%"+query+"%");
+			
+			ResultSet rs = st.executeQuery();
+			
+			count = rs.getInt("count");
+			
+			while(rs.next()) {
+				int id = rs.getInt("ID");
+				String title = rs.getString("TITLE");
+				Date regDate = rs.getDate("REGDATE");
+				String writerId = rs.getString("WRITER_ID");
+				String hit = rs.getString("HIT");
+				String files = rs.getString("FILES");
+				String content = rs.getString("CONTENT");
+				
+				Notice notice = new Notice(id,
+						title,
+						regDate,
+						writerId,
+						hit,
+						files,
+						content);
+				list.add(notice);
+			}
+			
+			rs.close();
+			st.close();
+			con.close();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		
-		
-		return 0;
+		return count;
 	}
 	
+	
+	//클릭한 공지의 세부 사항을 가져오는 함수
 	public Notice getNotice(int id) {
+		
+		Notice notice = null;
 		String sql = "select * from notice where id=?";
-		return null;
+		
+		List<Notice> list = new ArrayList<>();
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con = DriverManager.getConnection(url,"\"Weed\"", "southkorea1");
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setInt(1, id);
+			ResultSet rs = st.executeQuery();
+			
+			if(rs.next()) {
+				int nid = rs.getInt("ID");
+				String title = rs.getString("TITLE");
+				Date regDate = rs.getDate("REGDATE");
+				String writerId = rs.getString("WRITER_ID");
+				String hit = rs.getString("HIT");
+				String files = rs.getString("FILES");
+				String content = rs.getString("CONTENT");
+				
+				Notice nnotice = new Notice(nid,
+						title,
+						regDate,
+						writerId,
+						hit,
+						files,
+						content);
+				list.add(nnotice);
+			
+			}
+			
+			rs.close();
+			st.close();
+			con.close();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return notice;
 	}
 	
 	public Notice getNextNotice(int id) {
 		String sql = "select id from "
 				+ "(select * from notice order by id) "
-				+ "where id>3 and rownum = 1";
+				+ "where id>? and rownum = 1";
 		
-		return null;
+		Notice notice = null;
+		
+		List<Notice> list = new ArrayList<>();
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con = DriverManager.getConnection(url,"\"Weed\"", "southkorea1");
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setInt(1, id);
+			ResultSet rs = st.executeQuery();
+			
+			if(rs.next()) {
+				int nid = rs.getInt("ID");
+				String title = rs.getString("TITLE");
+				Date regDate = rs.getDate("REGDATE");
+				String writerId = rs.getString("WRITER_ID");
+				String hit = rs.getString("HIT");
+				String files = rs.getString("FILES");
+				String content = rs.getString("CONTENT");
+				
+				Notice nnotice = new Notice(nid,
+						title,
+						regDate,
+						writerId,
+						hit,
+						files,
+						content);
+				list.add(nnotice);
+			
+			}
+			
+			rs.close();
+			st.close();
+			con.close();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return notice;
 	}
 	
 	public Notice getprevNotice(int id) {
 		String sql = "select id from notice "
 				+ "where id = (select id from "
 				+ "(select * from notice order by id desc) "
-				+ "where id<3 and rownum = 1)";
-		return null;
+				+ "where id<? and rownum = 1)";
+
+
+		Notice notice = null;
+		
+		List<Notice> list = new ArrayList<>();
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con = DriverManager.getConnection(url,"\"Weed\"", "southkorea1");
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setInt(1, id);
+			ResultSet rs = st.executeQuery();
+			
+			if(rs.next()) {
+				int nid = rs.getInt("ID");
+				String title = rs.getString("TITLE");
+				Date regDate = rs.getDate("REGDATE");
+				String writerId = rs.getString("WRITER_ID");
+				String hit = rs.getString("HIT");
+				String files = rs.getString("FILES");
+				String content = rs.getString("CONTENT");
+				
+				Notice nnotice = new Notice(nid,
+						title,
+						regDate,
+						writerId,
+						hit,
+						files,
+						content);
+				list.add(nnotice);
+			
+			}
+			
+			rs.close();
+			st.close();
+			con.close();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return notice;
 	}
 
 }
